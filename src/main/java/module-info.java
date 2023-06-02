d04d8645136080ba0398886bcc1966d1f091e90a
Tiến Đạt <tiendat942003@gmail.com>
2023-06-02 12:39:44 +0700

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.quiztestapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.quiztestapp to javafx.fxml;
-    exports com.example.quiztestapp;
-}

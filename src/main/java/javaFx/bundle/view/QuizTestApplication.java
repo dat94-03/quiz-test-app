0c684bbe42891cc5c666d0492a0ce27762ac38a9
tiendatdev <tiendat942003@gmail.com>
2023-06-04 09:55:59 +0700

    rename

--- src/main/java/javaFx/bundle/view/QuizTestApplication.java
+++ src/main/java/javaFx/bundle/view/QuizTestApplication.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import model.AikenFormatReader;
 
 import java.io.IOException;
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
-module view {
+module com.example.ourproject {
     requires javafx.controls;
     requires javafx.fxml;
 
+
     opens javaFx.bundle.view to javafx.fxml;
     exports javaFx.bundle.view;
-
 }
